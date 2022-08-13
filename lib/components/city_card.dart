82988dfae409414613f76f40721fea51f644895e
Mehrdad Shokri <code.poet95@gmail.com>
2022-08-13 22:35:02 +0430

    wip

--- lib/components/city_card.dart
+++ lib/components/city_card.dart
@@ -20,144 +20,141 @@ class CityCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
         margin: shouldAddMargin ? const EdgeInsets.only(top: 16) : null,
-        child: Stack(
-          alignment: Alignment.topRight,
-          fit: StackFit.loose,
-          children: [
-            PlatformElevatedButton(
-              color: Colors.white,
-              onPressed: () {},
-              padding: Constants.CARD_INNER_PADDING,
-              material: (_, __) => MaterialElevatedButtonData(
-                  style: ButtonStyle(
-                      shape: MaterialStateProperty.all(RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(16),
-              )))),
-              cupertino: (_, __) => CupertinoElevatedButtonData(
-                  borderRadius: BorderRadius.circular(16)),
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Row(
-                    children: [
-                      Flexible(
-                        flex: 4,
-                        child: Text(weatherForecast.cityName ?? '',
-                            softWrap: false,
-                            overflow: TextOverflow.fade,
-                            style: TextStyle(
-                                color: textColor(context),
-                                fontWeight: Constants.MEDIUM_FONT_WEIGHT,
-                                fontSize: Constants.S1_FONT_SIZE)),
-                      ),
-                      const Spacer(
-                        flex: 1,
-                      ),
-                    ],
-                  ),
-                  const SizedBox(
-                    height: 4,
-                  ),
-                  if (strNotEmpty(weatherForecast.countryCode))
+        child: PlatformElevatedButton(
+            color: Colors.white,
+            onPressed: () {},
+            padding: Constants.CARD_INNER_PADDING,
+            material: (_, __) => MaterialElevatedButtonData(
+                    style: ButtonStyle(
+                        shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(16),
+                )))),
+            cupertino: (_, __) => CupertinoElevatedButtonData(
+                borderRadius: BorderRadius.circular(16)),
+            child: Stack(
+              alignment: Alignment.centerRight,
+              fit: StackFit.loose,
+              children: [
+                Positioned(
+                  child: Lottie.asset(
+                      'assets/lottie/${weatherForecast.lottieAnimation}.json',
+                      alignment: Alignment.center,
+                      width: 72,
+                      height: 72,
+                      fit: BoxFit.contain),
+                ),
+                Column(
+                  mainAxisSize: MainAxisSize.max,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
                     Row(
-                      mainAxisAlignment: MainAxisAlignment.start,
                       children: [
-                        Image.asset(
-                            'icons/flags/png/${weatherForecast.countryCode}.png',
-                            width: 24,
-                            height: 16,
-                            package: 'country_icons'),
-                        const SizedBox(
-                          width: 4,
+                        Flexible(
+                          flex: 4,
+                          child: Text(weatherForecast.cityName ?? '',
+                              softWrap: false,
+                              overflow: TextOverflow.fade,
+                              style: TextStyle(
+                                  color: textColor(context),
+                                  fontWeight: Constants.MEDIUM_FONT_WEIGHT,
+                                  fontSize: Constants.S1_FONT_SIZE)),
                         ),
-                        Text(weatherForecast.countryCode!.toUpperCase(),
-                            style: TextStyle(
-                                color: secondaryTextColor(context),
-                                fontWeight: Constants.REGULAR_FONT_WEIGHT,
-                                fontSize: Constants.CAPTION_FONT_SIZE)),
+                        const Spacer(
+                          flex: 1,
+                        ),
                       ],
                     ),
-                  const SizedBox(
-                    height: 8,
-                  ),
-                  if (weatherForecast.temp != null) ...[
-                    TemperatureIcon(
-                        unit: weatherForecast.unit,
-                        temperature: weatherForecast.temp!.toString()),
                     const SizedBox(
                       height: 4,
                     ),
-                  ],
-                  Text(
-                    weatherForecast.weatherTitle,
-                    style: TextStyle(
-                        fontSize: Constants.S2_FONT_SIZE,
-                        fontWeight: Constants.REGULAR_FONT_WEIGHT,
-                        color: secondaryTextColor(context)),
-                  ),
-                  const Spacer(),
-                  Row(
-                    children: [
+                    if (strNotEmpty(weatherForecast.countryCode))
                       Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
-                          SvgPicture.asset(
-                            'assets/svg/drop.svg',
-                            width: 16,
-                            height: 16,
-                            fit: BoxFit.contain,
-                            color: Colors.blue.shade400,
-                          ),
+                          Image.asset(
+                              'icons/flags/png/${weatherForecast.countryCode}.png',
+                              width: 24,
+                              height: 16,
+                              package: 'country_icons'),
                           const SizedBox(
                             width: 4,
                           ),
-                          Text(
-                            '${weatherForecast.humidityPercent}%',
-                            style: TextStyle(
-                                color: textColor(context),
-                                fontSize: Constants.CAPTION_FONT_SIZE,
-                                fontWeight: Constants.REGULAR_FONT_WEIGHT),
-                          )
+                          Text(weatherForecast.countryCode!.toUpperCase(),
+                              style: TextStyle(
+                                  color: secondaryTextColor(context),
+                                  fontWeight: Constants.REGULAR_FONT_WEIGHT,
+                                  fontSize: Constants.CAPTION_FONT_SIZE)),
                         ],
                       ),
-                      const Spacer(),
-                      Row(
-                        children: [
-                          SvgPicture.asset(
-                            'assets/svg/windspeed.svg',
-                            height: 16,
-                            width: 16,
-                            fit: BoxFit.contain,
-                            color: Colors.grey.shade600,
-                          ),
-                          const SizedBox(
-                            width: 4,
-                          ),
-                          Text(
-                            '${weatherForecast.windSpeed.toStringAsFixed(0)}${windSpeedUnit(weatherForecast.unit)}',
-                            style: TextStyle(
-                                color: textColor(context),
-                                fontSize: Constants.CAPTION_FONT_SIZE,
-                                fontWeight: Constants.REGULAR_FONT_WEIGHT),
-                          )
-                        ],
+                    const Spacer(),
+                    if (weatherForecast.temp != null) ...[
+                      TemperatureIcon(
+                          unit: weatherForecast.unit,
+                          temperature: weatherForecast.temp!.toString()),
+                      const SizedBox(
+                        height: 4,
                       ),
                     ],
-                  ),
-                ],
-              ),
-            ),
-            Positioned(
-              top: 4,
-              right: 4,
-              child: Lottie.asset(
-                  'assets/lottie/${weatherForecast.lottieAnimation}.json',
-                  alignment: Alignment.center,
-                  width: 72,
-                  height: 72,
-                  fit: BoxFit.contain),
-            )
-          ],
-        ));
+                    Text(
+                      weatherForecast.weatherTitle,
+                      style: TextStyle(
+                          fontSize: Constants.S2_FONT_SIZE,
+                          fontWeight: Constants.REGULAR_FONT_WEIGHT,
+                          color: secondaryTextColor(context)),
+                    ),
+                    const Spacer(),
+                    Row(
+                      children: [
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg/drop.svg',
+                              width: Constants.ICON_SMALL_SIZE,
+                              height: Constants.ICON_SMALL_SIZE,
+                              fit: BoxFit.contain,
+                            ),
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              '${weatherForecast.humidityPercent}%',
+                              style: TextStyle(
+                                  color: textColor(context),
+                                  fontSize: Constants.CAPTION_FONT_SIZE,
+                                  fontWeight: Constants.REGULAR_FONT_WEIGHT),
+                            )
+                          ],
+                        ),
+                        const Spacer(),
+                        Row(
+                          children: [
+                            SvgPicture.asset(
+                              'assets/svg/windspeed.svg',
+                              height: Constants.ICON_SMALL_SIZE,
+                              width: Constants.ICON_SMALL_SIZE,
+                              fit: BoxFit.contain,
+                              color: Colors.grey.shade600,
+                            ),
+                            const SizedBox(
+                              width: 4,
+                            ),
+                            Text(
+                              '${weatherForecast.humidityPercent.toStringAsFixed(0)}${windSpeedUnit(weatherForecast.unit)}',
+                              style: TextStyle(
+                                  color: textColor(context),
+                                  fontSize: Constants.CAPTION_FONT_SIZE,
+                                  fontWeight: Constants.REGULAR_FONT_WEIGHT),
+                            )
+                          ],
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 4,
+                    ),
+                  ],
+                ),
+              ],
+            )));
   }
 }
